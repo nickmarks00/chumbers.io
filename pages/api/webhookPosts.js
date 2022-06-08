0e8e7c43d3d21d4d9280291979ea63a83eab616a
nickmarks00 <nickmarks00@gmail.com>
2022-06-08 18:24:17 +1000

    Updated Vercel .env variables for Redis

--- pages/api/webhookPosts.js
+++ pages/api/webhookPosts.js
@@ -7,11 +7,11 @@ export default async function handler(req, res) {
   // TODO: destructure data to only get relevant props
 
   const body = req.body; // Typically req.body
-  // const signature = req.headers["gcms-signature"];
+  const signature = req.headers["gcms-signature"];
 
-  // const isValid = verifyWebhookSignature({ body, signature, secret });
+  const isValid = verifyWebhookSignature({ body, signature, secret });
 
-  if (true) {
+  if (isValid) {
     const id = await createPost(req.body.data);
     res.status(200).json({ id });
   }
