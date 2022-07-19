a59fe4e3a78252742974c4fa7c4971f71240f97a
nickmarks00 <nickmarks00@gmail.com>
2022-07-19 15:39:49 +1000

    Merge _document.jsx and .js

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -2,18 +2,29 @@ import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html>
-      <Head />
+    <Html lang="en">
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="true"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Playfair+Display:wght@400;700&family=Poppins:wght@300;400;500&display=optional"
+          rel="stylesheet"
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
+        <noscript
+          dangerouslySetInnerHTML={{
+            __html: `< src="https://www.googletagmanager.com/ns.html?id=GTM-KZXLFX7"
+            height="0" width="0" style="display:none;visibility:hidden"/>`,
+          }}
+        />
       </body>
-      <noscript
-        dangerouslySetInnerHTML={{
-          __html: `< src="https://www.googletagmanager.com/ns.html?id=GTM-KZXLFX7"
-        height="0" width="0" style="display:none;visibility:hidden"/>`,
-        }}
-      />
     </Html>
   );
 }
