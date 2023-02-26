3f5be37e423f23b58cf2aeb6b9098e61b7095b6a
Nick Marks <nickmarks00@gmail.com>
2023-02-26 12:20:32 -0500

    Fix script in head tag of content/slug

--- pages/content/[slug].js
+++ pages/content/[slug].js
@@ -37,23 +37,6 @@ const Content = ({ post }) => {
 
   const url = typeof window !== "undefined" ? window.location.href : "";
 
-  useEffect(() => {
-    document.addEventListener("DOMContentLoaded", function () {
-      renderMathInElement(document.body, {
-        // customised options
-        // • auto-render specific keys, e.g.:
-        delimiters: [
-          { left: "$$", right: "$$", display: true },
-          { left: "$", right: "$", display: false },
-          { left: "\\(", right: "\\)", display: false },
-          { left: "\\[", right: "\\]", display: true },
-        ],
-        // • rendering keys, e.g.:
-        throwOnError: false,
-      });
-    });
-  }, []);
-
   return (
     <>
       <Head>
@@ -62,18 +45,6 @@ const Content = ({ post }) => {
           href="https://cdn.jsdelivr.net/npm/katex@0.13.11/dist/katex.min.css"
           integrity="sha384-Um5gpz1odJg5Z4HAmzPtgZKdTBHZdw8S29IecapCSB31ligYPhHQZMIlWLYQGVoc"
           crossOrigin="anonymous"
-        />
-        <Script
-          defer
-          src="https://cdn.jsdelivr.net/npm/katex@0.16.4/dist/katex.min.js"
-          integrity="sha384-PwRUT/YqbnEjkZO0zZxNqcxACrXe+j766U2amXcgMg5457rve2Y7I6ZJSm2A0mS4"
-          crossorigin="anonymous"
-        />
-        <Script
-          defer
-          src="https://cdn.jsdelivr.net/npm/katex@0.16.4/dist/contrib/auto-render.min.js"
-          integrity="sha384-+VBxd3r6XgURycqtZ117nYw44OOcIax56Z4dCRWbxyPt0Koah1uHoK0o4+/RRE05"
-          crossorigin="anonymous"
         />
       </Head>
 
