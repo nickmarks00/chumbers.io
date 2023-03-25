c4698924fc26372259b426e8eb41e5e116fdd77a
Nick Marks <nickmarks00@gmail.com>
2023-03-25 18:17:57 -0400

    Update markdown scripting for Roam embeds

--- scripts/chumbers.sh
+++ scripts/chumbers.sh
@@ -15,8 +15,18 @@ outFile="${destFile}_cleaned.md"
 
 cp -- "$inFile" "${outFile}"
 
-
+# Handling custom highlighting MDX
 sed -i 's/\^\^\([^^]*\)\^\^/\<Highlight text="\1" \/\>/g' "$outFile"
 
-sed -i 's/{{\[\[video\]\]: \([^}]*\)}}\([a-zA-Z .]*\)/\<VideoEmbed url="\1" caption="\2"\/\>/g' "$outFile"
+# Handling video embed
+sed -i 's/{{\[\[video\]\]: \([^}]*\)}}\([a-zA-Z .!,]*\)/\<VideoEmbed url="\1" caption="\2"\/\>/g' "$outFile"
 
+# Handling Roam italics to Markdown italics
+sed -i 's/__\(.*\)__/\*\1\*/g' "$outFile"
+
+# Handling $$ environment for \begin types
+sed -i 's/\$\$\(\\begin.*\\end{.*}\)\$\$/\n\$\$\n\1\n\$\$/g' "$outFile"
+
+echo "File cleaned succesfully..."
+
+code "$outFile"
