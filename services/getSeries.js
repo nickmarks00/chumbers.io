8ff1e945c3c6d4a3f1b0a758a7a47e6b27adbcef
Nick Marks <nickmarks00@gmail.com>
2023-03-25 18:20:33 -0400

    Patch readingTime to detailed series view fetch

--- services/getSeries.js
+++ services/getSeries.js
@@ -2,64 +2,65 @@ import { gql } from "@apollo/client";
 import client from "../apollo-client";
 
 export const getSeriesPaths = async () => {
-  const result = await client.query({
-    query: gql`
-      query MyQuery {
-        allSeriesConnection {
-          edges {
-            cursor
-            node {
-              slug
+    const result = await client.query({
+        query: gql`
+        query MyQuery {
+            allSeriesConnection {
+                edges {
+                    cursor
+                    node {
+                        slug
+                    }
+                }
             }
-          }
         }
-      }
-    `,
-  });
+        `,
+    });
 
-  return result.data.allSeriesConnection.edges;
+    return result.data.allSeriesConnection.edges;
 };
 
 export const getSeriesSimple = async () => {
-  const result = await client.query({
-    query: gql`
-      query {
-        allSeries {
-          slug
-          updatedAt
+    const result = await client.query({
+        query: gql`
+        query {
+            allSeries {
+                slug
+                updatedAt
+            }
         }
-      }
-    `,
-  });
+        `,
+    });
 
-  return result.data.allSeries;
+    return result.data.allSeries;
 };
 
 export const getSingleSeries = async (slug) => {
-  const result = await client.query({
-    query: gql`
-      query GetSeries($slug: String!) {
-        series(where: { slug: $slug }) {
-          seriesTitle
-          slug
-          overview
-          posts {
-            featuredPost
-            slug
-            title
-            publishedAt
-            content
-            excerpt
-          }
-          seriesImage {
-            alternate
-            url
-          }
+    const result = await client.query({
+        query: gql`
+        query GetSeries($slug: String!) {
+            series(where: { slug: $slug }) {
+                seriesTitle
+                readingTime
+                slug
+                overview
+                posts {
+                    featuredPost
+                    slug
+                    title
+                    publishedAt
+                    content
+                    excerpt
+                }
+                seriesImage {
+                    alternate
+                    url
+                }
+            }
         }
-      }
-    `,
-    variables: { slug: slug },
-  });
+        `,
+        variables: { slug: slug },
+    });
 
-  return result.data.series;
+    return result.data.series;
 };
