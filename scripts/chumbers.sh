a82fae70cc71ad91d4fcc9d8805c0d87475fa693
Nick Marks <nickmarks00@gmail.com>
2023-06-20 19:08:07 +0200

    Fix Roam italics conversion

--- scripts/chumbers.sh
+++ scripts/chumbers.sh
@@ -22,7 +22,7 @@ sed -i 's/\^\^\([^^]*\)\^\^/\<Highlight text="\1" \/\>/g' "$outFile"
 sed -i 's/{{\[\[video\]\]: \([^}]*\)}}\([a-zA-Z .!,]*\)/\<VideoEmbed url="\1" caption="\2"\/\>/g' "$outFile"
 
 # Handling Roam italics to Markdown italics
-sed -i 's/__\(.*\)__/\*\1\*/g' "$outFile"
+sed -i 's/__\([^_]*\)__/\*\1\*/g' "$outFile"
 
 # Handling $$ environment for \begin types
 sed -i 's/\$\$\(\\begin.*\\end{.*}\)\$\$/\n\$\$\n\1\n\$\$/g' "$outFile"
